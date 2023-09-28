0c2697c2b8857ad8b1321d3524ef674d426a03c9
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-09-27 20:11:39 -0400

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBg: '#292b4d',
+        customBgLight: '#353766',
+        lightBlue: '#17d1e0',
+        customDarkBlue: '#30587b',
+        customPrimaryText: '#f1f1f4',
+        customText: '#9499c4',
+      },
+    },
   },
   plugins: [],
 }
