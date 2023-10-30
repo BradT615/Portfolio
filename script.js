baa784144ea7dfc2f778cc0b970d583710ec8598
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-10-29 21:12:15 -0400

    particle load sequence

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // script.js
+
 document.addEventListener('DOMContentLoaded', (event) => {
-    fetch('particles-config.json')
-      .then(response => response.json())
-      .then(config => {
-        particlesJS('particles-js', config);
-      });
-  });
-  
+  fetch('particles-config.json')
+    .then(response => response.json())
+    .then(config => {
+      particlesJS('particles-js', config);
+
+      // Reveal the main content after particles have loaded
+      const mainContent = document.getElementById('mainContent');
+      mainContent.style.opacity = "1";
+    });
+});
